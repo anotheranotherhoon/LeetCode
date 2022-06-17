5795c2763f8f0cbe5802e533ce85a0fd28bb6085
Changhoon Lee <77476348+anotheranotherhoon@users.noreply.github.com>
2022-06-17 21:23:07 +0900

    Time: 131 ms (7.50%), Space: 44.1 MB (80.66%) - LeetHub

---
+++ 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+    node.val = node.next.val
+    node.next.val = node.next.next.val
+    node.next.next = null
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function(node) {
+    while(node.next!==null){
+        node.val = node.next.val
+        if(node.next.next===null){
+            node.next = null
+        }else{
+            node = node.next
+        }
+        
+    }
+}

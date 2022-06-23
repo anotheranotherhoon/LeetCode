67af1c739adbe3281a0ce5299169c5b923693e3c
Changhoon Lee <77476348+anotheranotherhoon@users.noreply.github.com>
2022-06-23 09:05:08 +0900

    Time: 212 ms (93.90%), Space: 51.3 MB (49.64%) - LeetHub

---
+++ 326-power-of-three/326-power-of-three.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+    if(n===0) return false
+    else if(n===1) return true
+    else if(n%3!==0) return false
+    else{
+        return isPowerOfThree(n/3)
+    }
+};
